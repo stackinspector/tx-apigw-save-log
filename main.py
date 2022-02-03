5769f3e84e98d0d6c1463d6392366fa6386be321
stackinspector <stackinspector@126.com>
2022-02-04 06:27:59 +0800

    fix

--- main.py
+++ main.py
@@ -7,25 +7,26 @@ from proc import proc
 arg_parser = argparse.ArgumentParser(description="Collect logs of API gateway service of Tencent Cloud.")
 arg_parser.add_argument("-r", "--region", required=True, type=str)
 arg_parser.add_argument("-s", "--service-id", required=True, type=str)
+arg_parser.add_argument("-p", "--path", required=True, type=str)
 arg_parser.add_argument("-b", "--bucket", type=str)
-arg_parser.add_argument("-p", "--path", type=str)
 arg_parser.add_argument("-d", "--date", type=str, help="%Y-%m-%d")
 args = arg_parser.parse_args()
 
-if args.bucket is not None and args.path is None:
-    raise Exception("bucket name provided but path not provided")
-
+region = args.region
+service_id = args.service_id
+path = args.path
+bucket = args.bucket
+date = args.date
 secret_id = os.environ.get("TENCENTCLOUD_SECRETID")
 secret_key = os.environ.get("TENCENTCLOUD_SECRETKEY")
 
-apigw_client = get_apigw_client(secret_id, secret_key, args.region)
+apigw_client = get_apigw_client(secret_id, secret_key, region)
+filename, data = proc(apigw_client, service_id, date)
+full_path = os.path.join(path, filename)
 
-filename, data = proc(apigw_client, args.service_id, args.date)
-
-if args.bucket is None:
-    with open(filename, "xb") as file:
+if bucket is None:
+    with open(full_path, "xb") as file:
         file.write(data)
 else:
-    cos_client = get_cos_client(secret_id, secret_key, args.region)
-    path = (args.path + "/" + filename).replace("//", "/")
-    upload_file(cos_client, args.bucket, path, data)
+    cos_client = get_cos_client(secret_id, secret_key, region)
+    upload_file(cos_client, bucket, full_path, data)
